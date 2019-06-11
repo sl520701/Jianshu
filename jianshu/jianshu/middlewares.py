2beaacacfe195a09d2335bf26e564f5ebfc4daeb
小桪 <51231056+sl520701@users.noreply.github.com>
2019-06-11 09:58:11 +0800

    Update middlewares.py

--- jianshu/jianshu/middlewares.py
+++ jianshu/jianshu/middlewares.py
@@ -18,9 +18,10 @@ class SeleniumDownloadMiddleware():
         self.driver.get(request.url)
         time.sleep(1)
         self.driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
-        summitBtn = self.driver.find_element_by_class_name('btn log-in')
-        time.sleep(2)
-        summitBtn.click()
+        #下拉到底部点击获取更多按钮
+        #summitBtn = self.driver.find_element_by_class_name('btn log-in')
+        #time.sleep(2)
+        #summitBtn.click()
         source = self.driver.page_source
         # 把请求到source封装成一个response对象返回给引擎再给spider进行处理
         response = HtmlResponse(url=self.driver.current_url, body=source, request=request, encoding='utf-8')
